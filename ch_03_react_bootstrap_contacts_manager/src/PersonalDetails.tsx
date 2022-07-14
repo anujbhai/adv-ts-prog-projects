cf9940318ecf3705c6ecb68d16ff769b6e3c5a52
Anuj.Upadhyay <anuj.upadhyay@rsystems.com>
2022-07-14 23:53:46 +0530

    adv-ts-3-prog-03 - react bootstrap contacts manager | setup finished.

--- ch_03_react_bootstrap_contacts_manager/src/PersonalDetails.tsx
+++ ch_03_react_bootstrap_contacts_manager/src/PersonalDetails.tsx
@@ -4,8 +4,33 @@ import {
   Col,
   Row
 } from 'reactstrap';
+import { IPersonState } from './State';
 
-export default class PersonalDetails extends Component {
+interface IProps {
+  DefaultState: IPersonState
+}
+
+export default class PersonalDetails extends Component<IProps, IPersonState> {
+  private defaultState: Readonly<IPersonState>;
+
+  constructor(props: IProps) {
+    super(props);
+
+    this.defaultState = props.DefaultState;
+    this.state = props.DefaultState
+  }
+
+  private updateBinding = (event: any) => {
+    switch (event.target.id) {
+      case "firstName":
+        this.setState({FirstName: event.target.value});
+        break;
+      case "lastName":
+        this.setState({LastName: event.target.value});
+        break;
+    }
+  };
+
   public render() {
     return (
       <>
@@ -14,6 +39,38 @@ export default class PersonalDetails extends Component {
             <Row>
               <Col>
                 <h4 className="mb-3">Personal Details</h4>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <label htmlFor="firstName">First Name</label>
+              </Col>
+              <Col>
+                <label htmlFor="lastName">Last Name</label>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <input
+                  type="text"
+                  value={this.state.FirstName}
+                  onChange={this.updateBinding}
+                  placeholder="First Name"
+                  id="firstName"
+                  className="form-control"
+                />
+              </Col>
+              <Col>
+                <input
+                  type="text"
+                  value={this.state.LastName}
+                  onChange={this.updateBinding}
+                  placeholder="Last Name"
+                  id="lastName"
+                  className="form-control"
+                />
               </Col>
             </Row>
           </Col>
